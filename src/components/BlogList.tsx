a41c28f59a5048c1ff198d6040d589a4bdc87aa8
UMESH KUMAR <246810umesh@gmail.com>
2026-01-17 21:33:12 +0530

    Revert "Add styling requirements to README"
    This reverts commit d7a0d979ff4a045ad10cf0b1576b2d5b2da29162.

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,9 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBlogs } from "@/api/blogAPI";
+import BlogCard from "./BlogCard";
 
+export default function BlogList({
+  onSelect,
+}: {
+  onSelect: (id: number) => void;
+}) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["blogs"],
+    queryFn: getBlogs,
+  });
 
-const BlogList = () => {
+  if (isLoading) return <p>Loading blogs...</p>;
+  if (error) return <p>Error fetching blogs</p>;
+
   return (
-    <div>BlogList</div>
-  )
+    <div className="space-y-4">
+      {data?.map((blog) => (
+        <BlogCard key={blog.id} blog={blog} onClick={onSelect} />
+      ))}
+    </div>
+  );
 }
-
-export default BlogList
